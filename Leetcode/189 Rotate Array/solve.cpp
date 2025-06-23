c9382e401dcc33e52d027005d3e4d0b9c95ec911
Md Galib Hossain <mdgalib23@gmail.com>
2025-06-23 23:48:25 +0600

    space complexity optimize to O(1)

--- Leetcode/189 Rotate Array/solve.cpp
+++ Leetcode/189 Rotate Array/solve.cpp
@@ -42,11 +42,45 @@ ans[realIndex]=arr[i];
 printArray(ans,n);
 }
 
+
+
+void reverse(int i,int j,int a[]){
+while(i<j){
+    swap(a[i],a[j]);
+    i++;
+    j--;
+}
+}
+
+void optimizedLeftRotate(){
+    int n,k;cin>>n>>k;
+int arr[n];
+inputArray(arr,n);
+//1,2,3,4,5
+k=k%n;
+reverse(0,k-1,arr);
+reverse(k,n-1,arr);
+reverse(0,n-1,arr);
+printArray(arr,n);
+}
+
+void optimizedRightRotate(){
+    int n,k;cin>>n>>k;
+int arr[n];
+inputArray(arr,n);
+//1,2,3,4,5
+k=k%n;
+reverse(0,n-1,arr);
+reverse(0,k-1,arr);
+reverse(k,n-1,arr);
+printArray(arr,n);
+}
 int main()
 {
 
-    rightRotate();
+    optimizedRightRotate();
     return 0;
 }
-//O(n) time
-//O(n) space
+
+
+
