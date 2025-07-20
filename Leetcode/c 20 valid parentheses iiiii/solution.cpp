c24ec97f9030c8c694bb92aa0695583e71e99e12
md-galib-hossain <mdgalib23@gmail.com>
2025-07-20 20:25:39 +0600

    solve: 20

---
+++ Leetcode/c 20 valid parentheses iiiii/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+int main()
+{
+    string s;
+    cin >> s;
+    stack<int> st;
+    for (char c : s)
+    {
+
+        if (c == '(' || c == '{' || c == '[')
+        {
+            st.push(c);
+        }
+        else
+        {
+            if (st.empty()) {
+                    return false; 
+                }
+            else if (c == ')' && st.top() == '(')
+            {
+                st.pop();
+            }
+            else if (c == '}' && st.top() == '{')
+            {
+                st.pop();
+            }
+            else if (c == ']' && st.top() == '[')
+            {
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+    }
+    return st.empty();
+    return 0;
+}
